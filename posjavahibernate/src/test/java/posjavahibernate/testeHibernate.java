aa62c79bba862956f6f0aa356897ac735c090dc4
CHIP7 <CHIP7@DESKTOP-995CF5N>
2023-08-14 18:00:49 -0300

    salvando dados

--- posjavahibernate/src/test/java/posjavahibernate/testeHibernate.java
+++ posjavahibernate/src/test/java/posjavahibernate/testeHibernate.java
@@ -2,12 +2,25 @@ package posjavahibernate;
 
 import org.junit.Test;
 
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
 public class testeHibernate {
 
 	@Test
 	public void testeHibernateUtil() {
 
-		HibernateUtil.getEntityManager();
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		
+		pessoa.setNome("Calebe");
+		pessoa.setSobrenome("Werneck");
+		pessoa.setEmail("calebewerneck@hotmail.com");
+		pessoa.setLogin("cwc3d");
+		pessoa.setSenha("cwc3d");
+		
+		daoGeneric.salvar(pessoa);
 
 	}
 
