cbc1ad40b12d4c0cde5254ae2dce023c5a238a49
CHIP7 <CHIP7@DESKTOP-995CF5N>
2023-08-21 11:18:40 -0300

    Salvar

--- posjavahibernate/src/main/java/repository/IDaoUsuarioPessoaImpl.java
+++ posjavahibernate/src/main/java/repository/IDaoUsuarioPessoaImpl.java
@@ -1,5 +1,7 @@
 package repository;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
@@ -16,27 +18,45 @@ public class IDaoUsuarioPessoaImpl implements IDaoUsuarioPessoa {
 	public UsuarioPessoa consultarUsuario(String login, String senha) {
 		
 		
-		UsuarioPessoa usuarioPessoa = null;
-		
-		
-		EntityTransaction transaction = entityManager.getTransaction(); //Obejto de transação
-		transaction.begin();// Inicia uma transação
-		
-		
-		usuarioPessoa = (UsuarioPessoa) entityManager.createQuery("select u from UsuarioPessoa u where u.login = '" + login +"' and u.senha = '" + senha + "'").getSingleResult();
-		
-		
-		transaction.commit();// Grava no banco de dados
-		entityManager.close();
+		try {
+			
+			UsuarioPessoa usuarioPessoa = null;
+			
+			
+			EntityTransaction transaction = entityManager.getTransaction(); //Obejto de transação
+			transaction.begin();// Inicia uma transação
+			
+			
+			usuarioPessoa = (UsuarioPessoa) entityManager.createQuery("select u from UsuarioPessoa u where u.login = '" + login +"' and u.senha = '" + senha + "'").getSingleResult();
+			
+			
+			transaction.commit();// Grava no banco de dados
+			entityManager.close();
 
+			
+			
+			
+			return usuarioPessoa;
 		
+			
+            
+		}
+			
 		
+
+			
+		catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação: ", "Usuario ou Senha Errado!"));
+			e.printStackTrace();
+			
+		}
+		return null;
 		
-		return usuarioPessoa;
-	}
-	
+	}}
+			
 	
 
 	
 
-}
+
